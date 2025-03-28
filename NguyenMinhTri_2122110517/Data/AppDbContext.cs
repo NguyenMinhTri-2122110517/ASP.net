17eceb4e8277e81f39a5bf167f0c21eddfec8dc4
NguyenMinhTri-2122110517 <tringuyen28082k4@gmail.com>
2025-03-28 16:13:08 +0700

    btbuoi1

---
+++ NguyenMinhTri_2122110517/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NguyenMinhTri_2122110517.Model;
+
+namespace NguyenMinhTri_2122110517.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ NguyenMinhTri_2122110517/Model/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace NguyenMinhTri_2122110517.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public Double Price { get; set; }
+    }
+}
